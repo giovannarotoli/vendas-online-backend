b885bda7d6be2af06dc00a5db060c5c980a4d2d2
giovannarotoli <giovanarotolireis@gmail.com>
2023-06-16 09:40:24 -0300

    Create user com método POST

---
+++ src/user/user.controller.ts
@@ -0,0 +1,17 @@
+import { Body, Controller,Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
+
+@Controller('user')
+export class UserController {
+    // @Get()
+    // async getAllUsers(){
+    //     return JSON.stringify({ test: 'abc'});
+    // }
+    @Post()
+    async createUser(@Body()createUser: CreateUserDto){
+        return {
+            ...createUser,
+            password: undefined,
+        };
+    }
+}
